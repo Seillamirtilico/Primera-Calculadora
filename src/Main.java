43776a8e84e24e5f942b8b902bcd3269c2b0a5c2
Seillamirtilico <juliand.fuentesg@konradlorenz.edu.co>
2023-08-15 14:04:02 -0500

    se agrega salir y queda perfecto

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-         
+
         int operacion = 0;
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a tu calculadora\nA continuacion selecciona la operacion que deseas realizar\n|  _________________  |\n" +
                 "| | JO           0. | |\n" +
                 "| |_________________| |\n" +
@@ -20,16 +20,18 @@ public class Main {
                 "| | . | 0 | = | | / | |\n" +
                 "| |___|___|___| |___| |\n" +
                 "|_____________________|");
-        System.out.println("_________Menu__________\nSuma Ingresa (1)\nResta Ingresa (2)\nMultiplicacion Ingresa (3)\nDivision Ingresa (4)\nPotencia Ingresa (5)\nModulo Ingresa (6)");
-        operacion= sc.nextInt();
-        switch(operacion){
+        do {
+        System.out.println("_________Menu__________\nSuma Ingresa (1)\nResta Ingresa (2)\nMultiplicacion Ingresa (3)\nDivision Ingresa (4)\nPotencia Ingresa (5)\nModulo Ingresa (6)\nSalir Ingresa (7)");
+        operacion = sc.nextInt();
+
+        switch (operacion) {
             case 1:
                 System.out.println("Dame el primer numero para sumar ");
                 int sum1 = sc.nextInt();
                 System.out.println("Dame el segundo numero para sumar ");
                 int sum2 = sc.nextInt();
-                int ressum= sum1 + sum2;
-                System.out.println("el resultado de tu suma es "+ressum);
+                int ressum = sum1 + sum2;
+                System.out.println("el resultado de tu suma es " + ressum);
                 break;
             case 2:
                 System.out.println("dame el primer numero para restar ");
@@ -37,7 +39,7 @@ public class Main {
                 System.out.println("dame el segundo numero para restar ");
                 int res2 = sc.nextInt();
                 int resres = res1 - res2;
-                System.out.println("el resultado de tu resta es "+resres);
+                System.out.println("el resultado de tu resta es " + resres);
                 break;
             case 3:
                 System.out.println("dame el primer numero para multiplicar ");
@@ -45,7 +47,7 @@ public class Main {
                 System.out.println("dame el segundo numero para multiplicar ");
                 int mut2 = sc.nextInt();
                 int resmut = mut1 * mut2;
-                System.out.println("el resultado de tu multiplicacion es "+resmut);
+                System.out.println("el resultado de tu multiplicacion es " + resmut);
                 break;
             case 4:
                 System.out.println("dame el primer numero para dividir ");
@@ -53,33 +55,33 @@ public class Main {
                 System.out.println("dame el segundo numero para dividir ");
                 double div2 = sc.nextDouble();
                 double resdiv = div1 / div2;
-                System.out.println("el resultado de tu division es "+resdiv);
+                System.out.println("el resultado de tu division es " + resdiv);
                 break;
             case 5:
                 System.out.println("Dame el numero que quieres elevar ");
                 double pot1 = sc.nextInt();
                 System.out.println("Dame el numero por el cual quieres elevar ");
                 double pot2 = sc.nextInt();
-                double respot = Math.pow(pot1,pot2);
-                System.out.println("el resultado de tu numero elevad es "+respot);
+                double respot = Math.pow(pot1, pot2);
+                System.out.println("el resultado de tu numero elevad es " + respot);
                 break;
             case 6:
                 System.out.println("Dame un numero para sacar el modulo ");
                 int mod1 = sc.nextInt();
                 System.out.println("Dame un numero el cual dividir y sacar el modulo ");
                 int mod2 = sc.nextInt();
-                int resmod= mod1 %= mod2;
-                System.out.println("el modulo de tu operacion es "+resmod);
+                int resmod = mod1 %= mod2;
+                System.out.println("el modulo de tu operacion es " + resmod);
+                break;
+            case 7:
+                System.out.println("gracias por usar la calculadora:)");
                 break;
             default:
                 System.out.println("Numero Invalido");
 
 
-
-
-
         }
-
+        }while (operacion!=7);
 
 
     }
